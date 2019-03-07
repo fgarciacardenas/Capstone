073636ca6a256a0e799e58f15b896b0aa3d0f838
cesa280899 <47410292+cesa280899@users.noreply.github.com>
2019-03-07 13:52:46 -0500

    Update Videoreciever(commented).py

--- Videoreciever(commented).py
+++ Videoreciever(commented).py
@@ -6,24 +6,38 @@ import numpy as np
 from cv_bridge import CvBridge, CvBridgeError # Import cvbridge to allow convertions between ROS and Opencv
 from sensor_msgs.msg import Image       # Import the Image ROS message 
 
-br = CvBridge()      # Initialize cvbridge
+# Initialize cvbridge
+br = CvBridge()      
 
-def callback(data):  #define what the code should do when recieves a new message
+#define what the code should do when recieves a new message
+def callback(data):  
     try:
-        image = br.imgmsg_to_cv2(data,"bgr8") # declare frame as the convertion of ROS image to opencv image (BGR 8 bits)
-        resize_image = cv2.resize (image,none,fx=2,fy=2)  # resize the image in a factor of 2 for both hieght amd width
-        cv2.imshow("Video",resize_image)        # show the image  
+        # declare frame as the convertion of ROS image to opencv image (BGR 8 bits)
+        image = br.imgmsg_to_cv2(data,"bgr8") 
+        
+        # resize the image in a factor of 2 for both hieght amd width
+        resize_image = cv2.resize (image,none,fx=2,fy=2)  
+        
+        # show the image
+        cv2.imshow("Video",resize_image)         
         cv2.waitKey(1)
+        
     except CvBridgeError as e:
         print(e)
 
 def videoreciever():
-    rospy.init_node('reciever',anonymous = True)    # Initialize node as "reciever" 
-    rospy.Subscriber("video_topic",Image,callback)  # Set the node as a subscriber of images in "video_topic" 
+    # Initialize node as "reciever" 
+    rospy.init_node('reciever',anonymous = True)   
+    
+    # Set the node as a subscriber of images in "video_topic"
+    rospy.Subscriber("video_topic",Image,callback) 
     rospy.spin()
 
 if __name__ == '__main__':
     try:
-        videoreciever()                  # run the code 
-    except rospy.ROSInterruptException:  # Allow code to be interrupt via ROS        
+        # run the code 
+        videoreciever()         
+        
+    # Allow code to be interrupt via ROS 
+    except rospy.ROSInterruptException:         
         pass
